423eac5754995a02af46f477fa8d42cd3ea8e262
jose barboza <jrbarboza08@gmail.com>
2018-05-31 03:54:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,43 @@
+function forLoop(array){
+  
+  for (let i = 0; i < 25; i++){
+    
+   
+      
+   array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+      
+    
+   
+    
+  }
+   return array
+}
+
+
+
+
+
+function whileLoop(x){
+while (x > 0) {
+  console.log(--x)
+}
+return "done"
+}
+
+
+
+
+function doWhileLoop(array) {
+  
+  return Math.random() >= 0.5
+  
+}
+  
+  do{
+    
+    array.shift()
+    
+  }while (mayeTrue());
+  
+  return array
+  
